a20f1bb68c06a92fdb49f13ac9f926a7a516eba0
patrickbdevaney <79435941+patrickbdevaney@users.noreply.github.com>
2021-08-23 21:03:21 -0400

    deployment

---
+++ app/Models/workoutmodel.js
@@ -0,0 +1,40 @@
+const mongoose = require('mongoose');
+
+const Schema = mongoose.Schema;
+
+const schema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now,
+  },
+  
+  exercises: [
+    {
+      type: {
+        type: String,
+      },
+      name: {
+        type: String,
+      },
+      distance: {
+        type: Number,
+      },
+      weight: {
+        type: Number,
+      },
+      sets: {
+        type: Number,
+      },
+      reps: {
+        type: Number,
+      },
+      duration: {
+        type: Number,
+      },
+    }
+  ]
+});
+
+const Workout = mongoose.model('Workout', schema);
+
+module.exports = Workout;
